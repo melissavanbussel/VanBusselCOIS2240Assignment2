06b954da35b6a8ddd939e67d17549dc70ac8c7f5
unknown <melissavanbussel@trentu.ca>
2019-02-12 16:49:53 -0500

    Copied and pasted the code template given on Blackboard

--- VanBussel_Melissa_Assignment2/src/Test.java
+++ VanBussel_Melissa_Assignment2/src/Test.java
@@ -1,5 +1,10 @@
-public class Test {
+class Test {
     public static void main(String[] args) {
+        GeometricObject gObjectArray [] = new GeometricObject [4];
+//Complete your code here
 
+    }
+    private static void printAreaAndPerimeter(GeometricObject gObject) {
+//Complete your code here
     }
 }
